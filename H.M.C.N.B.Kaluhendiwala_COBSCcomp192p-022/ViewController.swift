133ee91b817feae723efcb9ae6ca94fea8bcccd1
charithakaluhendiwala <hmcnbkaluhendiwala@gmail.com>
2021-04-25 13:25:09 +0530

    Initial Commit

---
+++ H.M.C.N.B.Kaluhendiwala_COBSCcomp192p-022/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  H.M.C.N.B.Kaluhendiwala_COBSCcomp192p-022
+//
+//  Created by char1 on 4/25/21.
+//  Copyright © 2021 char1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
