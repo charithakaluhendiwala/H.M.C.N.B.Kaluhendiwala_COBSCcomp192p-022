2530092625f9ae85a5a5645aeb03f7c2496d03d1
charithakaluhendiwala <hmcnbkaluhendiwala@gmail.com>
2021-04-30 23:43:17 +0530

    Final Commit

--- H.M.C.N.B.Kaluhendiwala_COBSCcomp192p-022/TableViewCells/OrderSummaryTableViewCell.swift
+++ H.M.C.N.B.Kaluhendiwala_COBSCcomp192p-022/TableViewCells/OrderSummaryTableViewCell.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class OrderSummaryTableViewCell: UITableViewCell {
 
+    
+    @IBOutlet weak var lblorderID: UILabel!
+    @IBOutlet weak var lblItems: UILabel!
+    @IBOutlet weak var lblQty: UILabel!
+    @IBOutlet weak var lblOrderTotal: UILabel!
+    
+    class var reuseIdentifier: String {
+        return "OrderSummaryCellReusable"
+    }
+    
+    class var nibName: String {
+        return "OrderSummaryTableViewCell"
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -17,8 +31,25 @@ class OrderSummaryTableViewCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    }
+    
+    func configXIB(order: Order) {
+        lblorderID.text = order.orderID
+        
+        var foodNames: String = ""
+        var orderInfo: String = ""
+        var totalAmount: Double = 0
+        
+        for item in order.orderItems {
+//            print(item.item_name)
+            foodNames += "\n\(item.item_name)"
+            orderInfo += "\n1 X \(item.price) LKR"
+            totalAmount += 1 * item.price
+        }
+        
+        lblItems.text = foodNames
+        lblQty.text = orderInfo
+        lblOrderTotal.text = "Total : \(totalAmount)"
     }
     
 }
